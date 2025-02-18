2b30c8abfd2d513c7b8b601446104fb5b9f36936
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-18 13:20:54 +0100

    first commit

---
+++ jpkcom-allow-blocks.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: JPKCom Allow Block Types
+Description: Only allow certain types of blocks in Gutenberg for non admins.
+Version: 1.0.0
+Author: Jean Pierre Kolb <jpk@jpkc.com>
+Author URI: https://www.jpkc.com
+*/
+
+// https://developer.wordpress.org/block-editor/reference-guides/core-blocks/
+
+
+if ( !is_admin() ) {
+
+    function jpkcom_allowed_block_types( $allowed_block_types, $block_editor_context ): array {
+
+        $allowed_block_types = array(
+            'areoi/accordion-item',
+            'areoi/accordion',
+            'areoi/button',
+            'areoi/carousel-item',
+            'areoi/carousel',
+            'areoi/column-break',
+            'areoi/column',
+            'areoi/container',
+            'areoi/div',
+            'areoi/row',
+            'areoi/strip',
+            'core/gallery',
+            'core/heading',
+            'core/image',
+            'core/list-item',
+            'core/list',
+            'core/paragraph',
+        );
+    
+        return $allowed_block_types;
+    }
+
+    add_filter( 'allowed_block_types_all', 'jpkcom_allowed_block_types', 10, 2 );
+
+}
