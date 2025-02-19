ae8f05b0fa696f87c7538d6fc49520fc83df5582
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-19 09:03:44 +0100

    v1.0.2

--- jpkcom-allow-blocks.php
+++ jpkcom-allow-blocks.php
@@ -3,7 +3,7 @@
 Plugin Name: JPKCom Allow Block Types
 Plugin URI: https://www.jpkc.com/
 Description: Only allow certain types of blocks in Gutenberg for non admins.
-Version: 1.0.2
+Version: 1.0.3
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Requires at least: 6.7
@@ -14,34 +14,42 @@ GitHub Plugin URI: JPKCom/jpkcom-allow-blocks
 Primary Branch: main
 */
 
+if ( ! defined( constant_name: 'WPINC' ) ) {
+	die;
+}
+
 // https://developer.wordpress.org/block-editor/reference-guides/core-blocks/
 
 
 if ( !is_admin() ) {
 
-    function jpkcom_allowed_block_types( $allowed_block_types, $block_editor_context ): array {
+    if ( ! function_exists( function: 'jpkcom_allowed_block_types' ) ) {
 
-        $allowed_block_types = array(
-            'areoi/accordion-item',
-            'areoi/accordion',
-            'areoi/button',
-            'areoi/carousel-item',
-            'areoi/carousel',
-            'areoi/column-break',
-            'areoi/column',
-            'areoi/container',
-            'areoi/div',
-            'areoi/row',
-            'areoi/strip',
-            'core/gallery',
-            'core/heading',
-            'core/image',
-            'core/list-item',
-            'core/list',
-            'core/paragraph',
-        );
-    
-        return $allowed_block_types;
+        function jpkcom_allowed_block_types( $allowed_block_types, $block_editor_context ): array {
+
+            $allowed_block_types = array(
+                'areoi/accordion-item',
+                'areoi/accordion',
+                'areoi/button',
+                'areoi/carousel-item',
+                'areoi/carousel',
+                'areoi/column-break',
+                'areoi/column',
+                'areoi/container',
+                'areoi/div',
+                'areoi/row',
+                'areoi/strip',
+                'core/gallery',
+                'core/heading',
+                'core/image',
+                'core/list-item',
+                'core/list',
+                'core/paragraph',
+            );
+
+            return $allowed_block_types;
+        }
+
     }
 
     add_filter( 'allowed_block_types_all', 'jpkcom_allowed_block_types', 10, 2 );
